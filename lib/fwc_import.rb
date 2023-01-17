ce902a42a49fb51bb831242875b854a412bcd9c2
Enoch Tamulonis <yungboiiindigo@gmail.com>
2023-01-17 08:30:27 -0500

    Extracting table contents from pages of fwc report

--- lib/fwc_import.rb
+++ lib/fwc_import.rb
@@ -1,17 +1,17 @@
-class Walker
-    def intialize
-    end
-
-    def set_stuff(*args)
-        binding.pry
-    end
-end
-
 class FwcImport
+    HEADER_NAMES = ["County", "Date Collected", "Alongshore Inshore", 
+        "Offshore", "Site Location", "Collector"]
     def initialize(filepath)
         File.open(filepath, "rb") do |io|
             reader = PDF::Reader.new(io)
-            binding.pry
+            reader.pages.each do |page|
+              table_content = page.text.split("\n\n\n")[-1]
+              binding.pry
+            end
         end
+    end
+
+    def extract_data_from_page(page)
+        # Todo: Add logic to extract
     end
 end
