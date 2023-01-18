8637af2e0dca0b89fb78695607ae90339a7caf84
Enoch Tamulonis <enochtamulonis@gmail.com>
2023-01-18 18:37:50 -0500

    add more logic to format rows into data to create records with

--- lib/fwc_import.rb
+++ lib/fwc_import.rb
@@ -6,10 +6,12 @@ class FwcImport
             reader = PDF::Reader.new(io)
             reader.pages.each do |page|
               table_content = page.text.split("\n\n\n")[-1]
-              rows_by_county = table_content.split("\n\n")
-              county = rows_by_county[0].split("\n")[-1]
-              rows_by_county[1..-1].each do |row|
-                extract_data_from_row(row)
+              rows = table_content.split("\n\n")
+              county = rows[0].split("\n")[-1]
+              rows[1..-1].each do |row|
+                data = extract_data_from_row(row)
+                data[:county] = county
+                #FwcReport.create_or_find_by!(data)
               end
             end
         end
@@ -18,8 +20,15 @@ class FwcImport
     def extract_data_from_row(row)
         text_values = row.gsub("\n", "").split("   ").compact_blank
         county, date_collected, alongshore, offshore, location, collector = text_values
-        if text_valuese.size > 6
-            location += " " + text_values[-1].strip
-        end
+        data = {
+            county: county&.strip, 
+            date_collected: date_collected&.strip,
+            inshore_level: alongshore&.strip, 
+            offshore_level: offshore&.strip, 
+            site_location: location&.strip, 
+            collector: collector&.strip
+        }
+        data[:site_location] += text_values[-1]
+        data
     end
 end
